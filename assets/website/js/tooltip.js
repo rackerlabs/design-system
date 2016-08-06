04fa2c459f64ed6568aa359d3082f925ae80dda7
Andrew Raiford <drew@andrewraiford.com>
2016-08-06 07:51:07 -0500

    feat(tooltip): adds positioning logic to tooltip.js

--- assets/website/js/tooltip.js
+++ assets/website/js/tooltip.js
@@ -2,27 +2,55 @@ var attachTipTimer;
 var removeTipTimer;
 var hideTipTimeout = 1500;
 var toolTipPointerHeight = 8;
-var tooltipPointerWidth = 16;
+var tooltipPointerWidth = 15;
 var toolTipPointerSpace = 20;
 
 var removeTooltips = function() {
 	$('.ds-tooltip').remove();
 }
 
+
 var attachTooltip = function(params) {
 		var newLeft = params.left - toolTipPointerSpace - (tooltipPointerWidth/2) + (params.jqObj.width()/2);
 		var newTop =  params.top + toolTipPointerHeight + params.jqObj.height();
-		var tooltip = $('<div class="ds-tooltip invisible" style="top: '+newTop+'px; left: '+newLeft+'px">'+
+		var tooltip = $('<div class="ds-tooltip invisible" style="top: 0px; left: 0px">'+
+												'<div class="ds-tooltip-arrow top-left"></div>'+
 												'<div class="ds-tooltip-inner">'+
     											params.contents+
   											'</div>'+
 											'</div>');
+		var containerScrollPosition = params.container.scrollTop();
+		var screenWidth = $(window).width();
+		var screenHeight = $(window).height();
+		var tooltipWidth;
+		var tooltipHeight;
+		var tooltipArrowClass = 'ds-tooltip-arrow';
+
 		var timeOut = 0;
 		if(params.delay){
 			timeOut = eval(params.delay)*1000;
 		}
 		attachTipTimer = setTimeout(function(){
 			$('body').append(tooltip);
+
+			tooltipWidth = tooltip.width();
+			tooltipHeight = tooltip.height();
+
+			if(newTop + tooltipHeight - containerScrollPosition > screenHeight) {
+				newTop =  (params.top - tooltipHeight) - (toolTipPointerHeight+2);
+				tooltipArrowClass += '-bottom';
+			} else {
+				tooltipArrowClass += '-top';
+			}
+
+			if(newLeft + tooltipWidth > screenWidth) {
+				newLeft = params.left - tooltipWidth + toolTipPointerSpace + (tooltipPointerWidth/2) + (params.jqObj.width()/2);
+				tooltipArrowClass += '-right';
+			} else {
+				tooltipArrowClass += '-left';
+			}
+
+			tooltip.find('.ds-tooltip-arrow').addClass(tooltipArrowClass);
 			tooltip.css('left',newLeft).css('top',newTop).removeClass('invisible').addClass('visible')
 		}, timeOut);
 
@@ -52,6 +80,7 @@ $(document).ready(function() {
 		tooltip.delay = $(this).attr('data-delay');
 		tooltip.left = Math.round($(this).offset().left);
 		tooltip.top = Math.round($(this).offset().top);
+		tooltip.container = $('body');
 		tooltip.jqObj = $(this);
 		attachTooltip(tooltip);
 	});
