9a40f0cfb03b7d6db61c775c873fc696aaf318ac
grothues <elizabethannjensen@gmail.com>
2016-08-18 15:36:05 -0500

    more chart stuff

--- assets/js/demo-charts.js
+++ assets/js/demo-charts.js
@@ -78,58 +78,6 @@ var demoBarChartb = new Chart(ctx0b, {
 });
 
 /*--------------------------------------------------------------
-demoBubbleChart
---------------------------------------------------------------*/
-var ctx1 = document.getElementById("demoBubbleChart");
-var demoBubbleChart = new Chart(ctx1, {
-    type: 'bubble',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [
-                {
-                    x: 20,
-                    y: 30,
-                    r: 15
-                },
-                {
-                    x: 40,
-                    y: 10,
-                    r: 10
-                }
-            ],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-
-/*--------------------------------------------------------------
 demoLineChart
 --------------------------------------------------------------*/
 var ctx2 = document.getElementById("demoLineChart");
