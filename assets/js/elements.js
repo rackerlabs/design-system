0306dca95f9ba4523bcc6a834e9acef2a0c2671f
Mikey Hougland <mike2195@mq21hwg8wl.rackspace.corp>
2016-08-25 13:15:33 -0400

    Dropdown with typeahead

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -95,6 +95,138 @@ $(document).ready(function() {
     $('#contact3').selectmenu();
     $('#contact3-button').addClass('textInputError');
 
+    //dropdown selector w/ typeahead
+      $.widget( "custom.combobox", {
+        _create: function() {
+          this.wrapper = $( "<span>" )
+            .addClass( "custom-combobox" )
+            .insertAfter( this.element );
+
+          this.element.hide();
+          this._createAutocomplete();
+          this._createShowAllButton();
+        },
+
+        _createAutocomplete: function() {
+          var selected = this.element.children( ":selected" ),
+            value = selected.val() ? selected.text() : "";
+
+          this.input = $( "<input>" )
+            .appendTo( this.wrapper )
+            .val( value )
+            .attr( "title", "Title" )
+            .attr( "placeholder", "Type or select an option" )
+            .addClass( "custom-combobox-input ui-widget ui-widget-content ui-selectmenu-button ui-button ui-state-default" )
+            .autocomplete({
+              delay: 0,
+              minLength: 0,
+              source: $.proxy( this, "_source" )
+            })
+
+          this._on( this.input, {
+            autocompleteselect: function( event, ui ) {
+              ui.item.option.selected = true;
+              this._trigger( "select", event, {
+                item: ui.item.option
+              });
+            },
+
+            autocompletechange: "_removeIfInvalid"
+          });
+        },
+
+        _createShowAllButton: function() {
+          var input = this.input,
+            wasOpen = false;
+
+          $( "<span>" )
+            .attr( "tabIndex", -1 )
+            .tooltip()
+            .appendTo( this.wrapper )
+            .button({
+              icons: {
+                primary: "ui-icon-triangle-1-s"
+              },
+              text: false
+            })
+            .removeClass( "ui-corner-all" )
+            .addClass( "custom-combobox-toggle ui-corner-right" )
+            .on( "mousedown", function() {
+              wasOpen = input.autocomplete( "widget" ).is( ":visible" );
+            })
+            .on( "click", function() {
+              input.trigger( "focus" );
+
+              // Close if already visible
+              if ( wasOpen ) {
+                return;
+              }
+
+              // Pass empty string as value to search for, displaying all results
+              input.autocomplete( "search", "" );
+            });
+        },
+
+        _source: function( request, response ) {
+          var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+          response( this.element.children( "option" ).map(function() {
+            var text = $( this ).text();
+            if ( this.value && ( !request.term || matcher.test(text) ) )
+              return {
+                label: text,
+                value: text,
+                option: this
+              };
+          }) );
+        },
+
+        _removeIfInvalid: function( event, ui ) {
+
+          // Selected an item, nothing to do
+          if ( ui.item ) {
+            return;
+          }
+
+          // Search for a match (case-insensitive)
+          var value = this.input.val(),
+            valueLowerCase = value.toLowerCase(),
+            valid = false;
+          this.element.children( "option" ).each(function() {
+            if ( $( this ).text().toLowerCase() === valueLowerCase ) {
+              this.selected = valid = true;
+              return false;
+            }
+          });
+
+          // Found a match, nothing to do
+          if ( valid ) {
+            return;
+          }
+
+          // Remove invalid value
+          this.input
+            .val( "" )
+            .attr( "title", value + " didn't match any item" )
+            .tooltip( "open" );
+          this.element.val( "" );
+          this._delay(function() {
+            this.input.tooltip( "close" ).attr( "title", "" );
+          }, 2500 );
+          this.input.autocomplete( "instance" ).term = "";
+        },
+
+        _destroy: function() {
+          this.wrapper.remove();
+          this.element.show();
+        }
+      });
+
+      $( "#combobox" ).combobox();
+      $( "#toggle" ).on( "click", function() {
+        $( "#combobox" ).toggle();
+      });
+
+
     //dropdown buttons
     var page = $( "#page" );
 
