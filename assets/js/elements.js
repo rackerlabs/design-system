95382b0bb90d6ce29bc1bb38796994849042fa11
Mikey Hougland <mike2195@mq21hwg8wl.rackspace.corp>
2016-08-25 10:38:17 -0400

    Typeahead

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -21,6 +21,74 @@ $(document).ready(function() {
     var subtract_width = prefix_width + suffix_width;
     $('#setWidth').width(395-subtract_width);
 
+    //typeahead
+    $( function() {
+      var availableTags = [
+        "Bart Renner",
+        "Carmen Broomes",
+        "Elizabeth Grothues",
+        "Eric Weidner",
+        "Lane Fielder",
+        "Mikey Hougland",
+        "Tommy Shook",
+        "Ty Taylor"
+      ];
+      $( "#tags" ).autocomplete({
+        source: availableTags
+      });
+    } );
+
+    $( function() {
+      var availableTags = [
+        "Bart Renner",
+        "Carmen Broomes",
+        "Elizabeth Grothues",
+        "Eric Weidner",
+        "Lane Fielder",
+        "Mikey Hougland",
+        "Tommy Shook",
+        "Ty Taylor"
+      ];
+      function split( val ) {
+        return val.split( /,\s*/ );
+      }
+      function extractLast( term ) {
+        return split( term ).pop();
+      }
+
+      $( "#tags-multi" )
+        // don't navigate away from the field on tab when selecting an item
+        .on( "keydown", function( event ) {
+          if ( event.keyCode === $.ui.keyCode.TAB &&
+              $( this ).autocomplete( "instance" ).menu.active ) {
+            event.preventDefault();
+          }
+        })
+        .autocomplete({
+          minLength: 0,
+          source: function( request, response ) {
+            // delegate back to autocomplete, but extract the last term
+            response( $.ui.autocomplete.filter(
+              availableTags, extractLast( request.term ) ) );
+          },
+          focus: function() {
+            // prevent value inserted on focus
+            return false;
+          },
+          select: function( event, ui ) {
+            var terms = split( this.value );
+            // remove the current input
+            terms.pop();
+            // add the selected item
+            terms.push( ui.item.value );
+            // add placeholder to get the comma-and-space at the end
+            terms.push( "" );
+            this.value = terms.join( ", " );
+            return false;
+          }
+        });
+    } );
+
     //dropdown selector
     $('#contact1').selectmenu();
     $('#contact2').selectmenu();
