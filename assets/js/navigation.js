e26023084fde7846b5f79c8ec7844cdb944bca0b
Evan Nabors <evan@technabors.com>
2017-06-29 12:07:35 -0500

    refactor(nav): refactor in page nav
    Was generating double anchor tags, code simplified to resolve this
    issue.

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -3,8 +3,6 @@ var arrayLength = h2.length;
 var anchorList = document.getElementById("anchor-list");
 for (var i = 0; i < arrayLength; i++) { 
 	var li = document.createElement("LI");
-	var aTag = document.createElement("A");
-	aTag.innerHTML = '<a href="#' + h2[i].id + '">' + h2[i].textContent + '</a>';
-	li.appendChild(aTag);
+	li.innerHTML = '<a href="#' + h2[i].id + '">' + h2[i].textContent + '</a>';
 	anchorList.appendChild(li);
 };
