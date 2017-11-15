6438ffa4ac11b92679085a76f5e03e814ad9aa0c
Laura Santamaria <laura.santamaria@rackspace.com>
2017-11-15 15:45:49 -0600

    more work

--- assets/js/codeContainer.js
+++ assets/js/codeContainer.js
@@ -1,1 +1,14 @@
-$()
+var CODELIB = CODELIB || ( function() {
+  var _args = {}; // private
+
+  return {
+      init : function(Args) {
+        _args = Args;
+      },
+      codeContainer : function() {
+        $.get("https://raw.githubusercontent.com/rackerlabs/helix-ui/master/source/components/" + _args[0] + "/index.html", function( data) {
+          $( ".code-container" ).append(data);
+        }, "text");
+      }
+    };
+}());
