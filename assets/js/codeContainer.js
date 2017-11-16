97586c4e0248753629143816b70f6986d9365e1e
Laura Santamaria <laura.santamaria@rackspace.com>
2017-11-16 14:42:29 -0600

    more work

--- assets/js/codeContainer.js
+++ assets/js/codeContainer.js
@@ -6,8 +6,13 @@ var CODELIB = CODELIB || ( function() {
         _args = Args;
       },
       codeContainer : function() {
-        $.get("https://raw.githubusercontent.com/rackerlabs/helix-ui/master/source/components/" + _args[0] + "/index.html", function( data) {
-          $( ".code-container" ).append(data);
+        $.get("https://raw.githubusercontent.com/rackerlabs/helix-ui/master/source/components/" + _args[0] + "/index.html", function( data ) {
+          // console.log(data);
+          // $(data).find(".demo").appendTo( ".code-container" );
+          var re = new RegExp("(-{3})([\s\S]*?)({% block content %})");
+          var cleanedData = data.replace(re,"");
+          console.log(cleanedData);
+          $( ".code-container" ).append( cleanedData );
         }, "text");
       }
     };
