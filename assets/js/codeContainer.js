2749bcdf6cf7b65b4f98c7a79b29546c76fa3b32
Laura Santamaria <laura.santamaria@rackspace.com>
2017-11-17 09:26:15 -0600

    cleaning up unused variable in new js file

--- assets/js/codeContainer.js
+++ assets/js/codeContainer.js
@@ -9,7 +9,6 @@ var CODELIB = CODELIB || ( function() {
       },
       codeContainer : function() {
         $.get("https://raw.githubusercontent.com/rackerlabs/helix-ui/master/source/components/" + _args[0] + "/index.html", function( data ) {
-          var re = new RegExp("-{3}[\s\S]*?{% block content %}");
           var mappedClean = {
             obj1:/{% code 'html' %}[\s\S]*?{% endcode %}/gi,
             obj2:/{% endblock %}/gi,
