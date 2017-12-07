8458b646c0e356b63a540fccc791e821464a3f6a
Laura Santamaria <laura.santamaria@rackspace.com>
2017-12-07 14:35:31 -0600

    fixing the side nav, too; ref issue #337

--- assets/js/wayfinding.js
+++ assets/js/wayfinding.js
@@ -18,8 +18,24 @@ var dict = {
   "Tables": "tables"
 };
 
+var getSiblings = function (elem) {
+    var siblings = [];
+    var sibling = elem.parentNode.firstChild;
+    for (; sibling; sibling = sibling.nextSibling) {
+        if (sibling.nodeType !== 1 || sibling === elem) continue;
+        siblings.push(sibling);
+    }
+    return siblings;
+};
+
 for (var key in dict){
   if (prevUrl.includes(dict[key])) {
+    var famAnchor = document.getElementById(dict[key]);
+    var sibAnchor = getSiblings(famAnchor);
+    for (var elem in sibAnchor) {
+      sibAnchor[elem].classList.remove("current");
+    }
+    famAnchor.className = "current";
     anchor.innerHTML = key;
   }
 }
