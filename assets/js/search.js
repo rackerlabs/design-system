a348de758d4e94c3e656aaf5e7fa913c81e8ef2e
Laura Santamaria <laura.santamaria@rackspace.com>
2017-12-11 18:50:51 -0600

    basic ranking implemented, cleaned up css, added counts, autopopulate search box, fixed no entry

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,6 +3,8 @@
 var results = [];
 
 var searchpg = document.getElementById("search-results");
+var searchBox = document.getElementById("search-box");
+var searchcount = document.getElementById("search-count");
 
 // Take in query string
 
@@ -12,8 +14,7 @@ function getUrlVars()
 {
   var vars = [], hash;
   var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-  for(var i = 0; i < hashes.length; i++)
-  {
+  for(var i = 0; i < hashes.length; i++) {
     hash = hashes[i].split('=');
     vars.push(hash[0]);
     vars[hash[0]] = hash[1];
@@ -23,6 +24,11 @@ function getUrlVars()
 //endref
 
 var queried = getUrlVars().q;
+var queriedupper = queried.replace(queried[0], queried[0].toUpperCase());
+var escaped = escape(queried);
+var queried2 = new RegExp("\\b" + queried + "\\b", 'i');
+
+var re = new RegExp("\\b" + queried + "\\b", 'gi');
 
 // Call the JSON feed as an object
 
@@ -39,30 +45,71 @@ jsonCall("feed.json").done(function(data,textStatus,jqXHR) {
 
   // Loop through the content_text kv pairs to find the query string.
   docArray.forEach(function(element) {
-    // TODO: Identify how many times the string appears in each kv pairs
-
     var fulltext = element.content_text;
 
     // Add the item to a dictionary
-    if (fulltext.match(queried)) {
+    if (fulltext.match(queried2)) {
+      var matchd = fulltext.match(queried2);
+      // DONE: Identify how many times the string appears in each kv pairs
+      if (fulltext.match(re)) {
+        ranking = fulltext.match(re);
+        rankd = ranking.length;
+      }
+      else {
+        const rankd = 1;
+      }
+      if (matchd.index > 50){
+        preindex = matchd.index - 50;
+      } else {
+        preindex = 0;
+      }
+      if (matchd.index + 50){
+        postindex = matchd.index + 50;
+      } else {
+        postindex = matchd.length;
+      }
+      if (matchd.input.slice(preindex, postindex)){
+        snipd = matchd.input.slice(preindex, postindex);
+      } else {
+        snipd = "No relevant text here.";
+      }
+      snipd = snipd.replace(queried, `<b>${queried}</b>`);
+      snipd = snipd.replace(queriedupper, `<b>${queriedupper}</b>`);
       results.push({
         "id": element.id,
         "title": element.title,
         "url": element.url,
         "summary": element.summary,
-        "hero-image": element.image
+        "hero-image": element.image,
+        "snippet": snipd,
+        "rank": rankd
       });
 
-      // TODO: Add weighting based on how many times the string appears
     }
   });
+  if (!queried || queried == undefined){
+    return;
+  } else {
+    // Add search query as prefilled in the search box
+    searchBox.value = escaped;
 
-  // Return the dictionary to the search page as an HTML object
-  results.forEach(function(result) {
-    //ref: https://stackoverflow.com/questions/595808/is-it-possible-to-append-to-innerhtml-without-destroying-descendants-event-list#comment407853_595825
-    var resultList = document.createElement("li");
-    resultList.innerHTML = `<a class='result-title hxContainerTitle' href="${result.url}">${result.title}</a><br/><span class='result-summary hxSubBody'>${result.summary}</span>`;
-    searchpg.appendChild(resultList);
-    //endref
-  });
+    searchcount.innerHTML=`Your search returned ${results.length} results.`;
+
+    // DONE: Add weighting based on how many times the string appears
+    results = results.sort(function (a,b){
+      return a.rank - b.rank;
+    });
+    results = results.reverse();
+    results.forEach(function(element){
+      return;
+    });
+    // Return the dictionary to the search page as an HTML object
+    results.forEach(function(result) {
+      //ref: https://stackoverflow.com/questions/595808/is-it-possible-to-append-to-innerhtml-without-destroying-descendants-event-list#comment407853_595825
+      var resultList = document.createElement("li");
+      resultList.innerHTML = `<p><a class='result-title hxContainerTitle' href="${result.url}">${result.title}</a></p><p class='result-summary hxSubdued'>${result.snippet}</p><p class='result-count hxSubBody'>There were ${result.rank} instances of <i>${escaped}</i> on this page.</p>`;
+      searchpg.appendChild(resultList);
+      //endref
+    });
+  }
 });
