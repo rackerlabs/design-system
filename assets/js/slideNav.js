075a1d4eba273429673e7658e27451cf12d1240a
Laura Santamaria <laura.santamaria@rackspace.com>
2017-12-19 16:16:40 -0600

    getting started on the web component through tutorials; lots of copy-pasta

---
+++ assets/js/slideNav.js
@@ -0,0 +1,82 @@
+/* jshint esversion: 6 */
+
+//TODO: Create perm open attr.
+//TODO: Create disabled attr.
+//TODO: Create slide open/closed attr.
+//TODO: Decide on overlay over content or shift content for slide open/closed.
+//TODO: Set perm open and disabled for large breakpoints.
+//TODO: Set slide open/closed attr for small breakpoints.
+
+class FakeHXSlideNav extends HTMLElement {
+  // A getter/setter for an open property.
+  get open() {
+    return this.hasAttribute('open');
+  }
+
+  set open(val) {
+    // Reflect the value of the open property as an HTML attribute.
+    if (val) {
+      this.setAttribute('open', '');
+    } else {
+      this.removeAttribute('open');
+    }
+    this.toggleDrawer();
+  }
+
+  // A getter/setter for a disabled property.
+  get disabled() {
+    return this.hasAttribute('disabled');
+  }
+
+  set disabled(val) {
+    // Reflect the value of the disabled property as an HTML attribute.
+    if (val) {
+      this.setAttribute('disabled', '');
+    } else {
+      this.removeAttribute('disabled');
+    }
+  }
+
+  // Can define constructor arguments if you wish.
+  constructor() {
+    // If you define a ctor, always call super() first!
+    // This is specific to CE and required by the spec.
+    super();
+
+    // Setup a click listener on <app-drawer> itself.
+    this.addEventListener('click', e => {
+      // Don't toggle the drawer if it's disabled.
+      if (this.disabled) {
+        return;
+      }
+      this.toggleDrawer();
+    });
+  }
+
+  // connectedCallback() {
+  //   if
+  // }
+
+  // disconnectedCallback() {
+  //   ...
+  // }
+  //
+  // Only called for the disabled and open attributes due to observedAttributes
+  attributeChangedCallback(name, oldValue, newValue) {
+    // When the drawer is disabled, update keyboard/screen reader behavior.
+    if (this.disabled) {
+      this.setAttribute('tabindex', '-1');
+      this.setAttribute('aria-disabled', 'true');
+    } else {
+      this.setAttribute('tabindex', '0');
+      this.setAttribute('aria-disabled', 'false');
+    }
+    // TODO: also react to the open attribute changing.
+  }
+
+  toggleDrawer() {
+    this.open();
+  }
+}
+
+customElements.define('fake-hx-slide-nav', FakeHXSlideNav);
